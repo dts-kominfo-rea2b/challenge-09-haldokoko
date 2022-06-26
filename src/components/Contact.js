788e2b046b34e2a6e525dd296d6dcfd12d55e14d
Coim Stark <alvarolavoizier.com>
2022-06-26 13:49:36 +0800

    bismillah

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,22 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
+    const {data} = props;
     return (
-        <></>
+        <div className='box'>
+            <aside>
+            <div className='itemPhoto'>
+                <img src={data.photo} ></img>
+            </div></aside>
+            <div>
+                <h3>{data.name}</h3>
+                
+                <p>{data.phone}</p>
+            
+                <p>{data.email}</p>
+            </div>
+        </div>        
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+        <header>
+        <h1 className='judul'>Call a Friend</h1>
+        <p className='sub'>your friendly contact app</p>
+        
+    </header>
     )
 }
 
